08a4a90c0ef1e914c3306f7f6e00f095efc47f72
UnfullStack <zacharyalbers@outlook.com>
2025-12-06 14:23:03 -0600

    Add files via upload

---
+++ pcon/test.py
@@ -0,0 +1,16 @@
+from js import document
+import requests
+
+wlbl = document.getElementById("lbl_wait")
+
+link = "NONE"
+
+try:
+    response = requests.get(pastebin_raw_link)
+    response.raise_for_status()
+    link = response.text
+
+except requests.exceptions.RequestException as e:
+    wlbl.innerHTML = f"Error contacting host: {e}"
+
+wlbl.innerHTML = f"Error contacting host: {e}"
